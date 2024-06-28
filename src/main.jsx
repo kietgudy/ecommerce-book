5147fc0cbf53d0680a6324f74ac66efd6290d0ea
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-06-28 19:08:33 +0700

    set up router

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
