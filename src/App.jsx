fa31ca6c7259ce90c75f9a7bfaf9678a901b10ba
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-06-28 19:25:14 +0700

    update layout router

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import React, { useState } from 'react';
-import LoginPage from './pages/login';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
+import React from "react";
+import LoginPage from "./pages/login";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import ContactPage from "./pages/contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 const Layout = () => {
-  return (
-    <>
-    main
-    </>
-  )
-}
+  return <div className="layout">
+  <Header/>
+  <Outlet/>
+  <Footer/>
+  </div>;
+};
 
 export default function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Layout />,
-      errorElement: <div>404</div>
+      errorElement: <div>404</div>,
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "contact",
+          element: <ContactPage />,
+        },
+      ],
     },
     {
       path: "/login",
-      element: <LoginPage/>,
+      element: <LoginPage />,
     },
   ]);
   return (
     <>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </>
   );
 }
