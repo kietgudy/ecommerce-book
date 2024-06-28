9f1a5eb50ebd9f625a181156e167b445ff2b8352
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-06-28 22:13:12 +0700

    axios custom

---
+++ src/utils/axiosCustomize.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_BACKEND_URL;
+const instance = axios.create({
+  baseURL: baseURL,
+  });
+  instance.interceptors.request.use(function (config) {
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+instance.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+  export default instance;
