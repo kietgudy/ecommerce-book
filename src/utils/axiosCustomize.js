d4fe26321863c844f2927e3573f94cafba38ff0d
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-06-29 17:35:44 +0700

    Handle token refresh when access token expires

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -5,20 +5,50 @@ const baseURL = import.meta.env.VITE_BACKEND_URL;
 const instance = axios.create({
   baseURL: baseURL,
   withCredentials: true, // set refesh token cookie(axios)
-  });
-
-  instance.defaults.headers.common = {'Authorization': `Bearer ${localStorage.getItem("access_token")}`}
-
-  instance.interceptors.request.use(function (config) {
-  return config;
-}, function (error) {
-  return Promise.reject(error);
 });
 
-instance.interceptors.response.use(function (response) {
-  return response && response.data ? response.data : response;
-}, function (error) {
-  return error?.response?.data ?? Promise.reject(error);
-});
+instance.defaults.headers.common = {
+  Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+};
 
-  export default instance;
+const handleRefreshToken = async () => {
+  const res = await instance.get("/api/v1/auth/refresh");
+  if (res && res.data) return res.data.access_token;
+  else null;
+};
+
+instance.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+const NO_RETRY_HEADER = "x-no-retry"; //prevent loop
+
+instance.interceptors.response.use(
+  function (response) {
+    return response && response.data ? response.data : response;
+  },
+  async function (error) {
+    if (
+      error.config &&
+      error.response &&
+      +error.response.status === 401 &&
+      !error.config.headers[NO_RETRY_HEADER]
+    ) {
+      const access_token = await handleRefreshToken();
+      error.config.headers[NO_RETRY_HEADER] = "true";
+      if (access_token) {
+        error.config.headers["Authorization"] = `Bearer ${access_token}`;
+        localStorage.setItem("access_token", access_token);
+        return instance.request(error.config);
+      }
+    }
+    return error?.response?.data ?? Promise.reject(error);
+  }
+);
+
+export default instance;
