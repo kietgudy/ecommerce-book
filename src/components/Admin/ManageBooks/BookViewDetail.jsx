e68e5213389249461eb45e503e0c52fdc85e8f29
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-06-30 23:11:47 +0700

    antd preview img

--- src/components/Admin/ManageBooks/BookViewDetail.jsx
+++ src/components/Admin/ManageBooks/BookViewDetail.jsx
@@ -1,14 +1,9 @@
 import React, { useState } from "react";
-import { Badge, Descriptions, Drawer } from "antd";
+import { Badge, Descriptions, Divider, Drawer, Modal, Upload } from "antd";
 import moment from "moment/moment";
 
 const BookViewDetail = (props) => {
-  const [open, setOpen] = useState(false);
-  const {
-    openViewDetail,
-    setOpenViewDetail,
-    dataViewDetail,
-  } = props;
+  const { openViewDetail, setOpenViewDetail, dataViewDetail } = props;
   const onClose = () => {
     setOpenViewDetail(false);
   };
@@ -17,6 +12,60 @@ const BookViewDetail = (props) => {
       return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + " đ";
     }
     return "";
+  };
+
+  const getBase64 = (file) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
+  const [previewTitle, setPreviewTitle] = useState("");
+  const [fileList, setFileList] = useState([
+    {
+      uid: "-1",
+      name: "image.png",
+      status: "done",
+      url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    },
+    {
+      uid: "-2",
+      name: "image.png",
+      status: "done",
+      url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    },
+    {
+      uid: "-3",
+      name: "image.png",
+      status: "done",
+      url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    },
+    {
+      uid: "-4",
+      name: "image.png",
+      status: "done",
+      url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    },
+  ]);
+  const handleCancel = () => setPreviewOpen(false);
+
+  const handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.url || file.preview);
+    setPreviewOpen(true);
+    setPreviewTitle(
+      file.name || file.url.substring(file.url.lastIndexOf("/") + 1)
+    );
+  };
+
+  const handleChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
   };
 
   return (
@@ -50,6 +99,23 @@ const BookViewDetail = (props) => {
             {moment(dataViewDetail?.updatedAt).format("DD-MM-YYYY HH:mm:ss")}
           </Descriptions.Item>
         </Descriptions>
+        <Divider orientation="left"> Ảnh Books </Divider>
+        <Upload
+          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+          listType="picture-card"
+          fileList={fileList}
+          onPreview={handlePreview}
+          onChange={handleChange}
+          showUploadList={{ showRemoveIcon: false }}
+        ></Upload>
+        <Modal
+          open={previewOpen}
+          title={previewTitle}
+          footer={null}
+          onCancel={handleCancel}
+        >
+          <img alt="example" style={{ width: "100%" }} src={previewImage} />
+        </Modal>
       </Drawer>
     </>
   );
