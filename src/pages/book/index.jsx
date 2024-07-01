b06466866b08646e7324dafd0d01f253be217d42
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-07-01 23:01:57 +0700

    fetch detail book

--- src/pages/book/index.jsx
+++ src/pages/book/index.jsx
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import ViewDetail from "../../components/Book/ViewDetail";
+import { callFetchBookById } from "../../services/api";
 
 const BookPage = () => {
+  const [dataBook, setDataBook] = useState();
   let location = useLocation();
   let params = new URLSearchParams(location.search);
   const id = params?.get("id");
-  return <div>
-    <ViewDetail/>
-  </div>;
+
+  useEffect(() => {
+    fetchBook(id);
+  }, [id]);
+
+  const fetchBook = async (id) => {
+    const res = await callFetchBookById(id);
+    if (res && res.data) {
+      let raw = res.data;
+      raw.items = getImages(raw);
+      setTimeout(() => {
+        setDataBook(raw);
+      }, 1000);
+    }
+  };
+
+  const getImages = (raw) => {
+    const images = [];
+    if (raw.thumbnail) {
+      images.push({
+        original: `${import.meta.env.VITE_BACKEND_URL}/images/book/${
+          raw.thumbnail
+        }`,
+        thumbnail: `${import.meta.env.VITE_BACKEND_URL}/images/book/${
+          raw.thumbnail
+        }`,
+        originalClass: "original-image",
+        thumbnailClass: "thumbnail-image",
+      });
+    }
+    if (raw.slider) {
+      raw.slider?.map((item) => {
+        images.push({
+          original: `${import.meta.env.VITE_BACKEND_URL}/images/book/${item}`,
+          thumbnail: `${import.meta.env.VITE_BACKEND_URL}/images/book/${item}`,
+          originalClass: "original-image",
+          thumbnailClass: "thumbnail-image",
+        });
+      });
+    }
+    return images;
+  };
+
+  return (
+    <>
+      <ViewDetail dataBook={dataBook} />
+    </>
+  );
 };
 
 export default BookPage;
--- src/services/api.js
+++ src/services/api.js
@@ -84,12 +84,33 @@ export const callCreateBook = (
   });
 };
 
-export const callUpdateBook = (id, thumbnail, slider, mainText, author, price, sold, quantity, category) => {
+export const callUpdateBook = (
+  id,
+  thumbnail,
+  slider,
+  mainText,
+  author,
+  price,
+  sold,
+  quantity,
+  category
+) => {
   return axios.put(`/api/v1/book/${id}`, {
-    thumbnail, slider, mainText, author, price, sold, quantity, category
-  })
-}
+    thumbnail,
+    slider,
+    mainText,
+    author,
+    price,
+    sold,
+    quantity,
+    category,
+  });
+};
 
 export const callDeleteBook = (id) => {
-  return axios.delete(`/api/v1/book/${id}`)
-}
+  return axios.delete(`/api/v1/book/${id}`);
+};
+
+export const callFetchBookById = (id) => {
+  return axios.get(`api/v1/book/${id}`);
+};
