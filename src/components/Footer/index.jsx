73858b69911bfcd43a87a28afc19c50c333e0434
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-07-03 20:19:11 +0700

    footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+import "./Footer.scss";
+import { FacebookFilled, FacebookOutlined, GithubOutlined, TwitterOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div class="container">
+        <div class="row">
+          <div class="footer-col">
+            <h4>company</h4>
+            <ul>
+              <li>
+                <a href="#">about us</a>
+              </li>
+              <li>
+                <a href="#">our services</a>
+              </li>
+              <li>
+                <a href="#">privacy policy</a>
+              </li>
+              <li>
+                <a href="#">affiliate program</a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>get help</h4>
+            <ul>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">shipping</a>
+              </li>
+              <li>
+                <a href="#">returns</a>
+              </li>
+              <li>
+                <a href="#">order status</a>
+              </li>
+              <li>
+                <a href="#">payment options</a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>online shop</h4>
+            <ul>
+              <li>
+                <a href="#">watch</a>
+              </li>
+              <li>
+                <a href="#">bag</a>
+              </li>
+              <li>
+                <a href="#">shoes</a>
+              </li>
+              <li>
+                <a href="#">dress</a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>follow us</h4>
+            <div class="social-links">
+              <Link style={{fontSize: 22}} to="https://www.facebook.com/tuannkiet1701" target="_blank">
+              <FacebookFilled />
+              </Link>
+              <Link style={{fontSize: 22}} to="https://github.com/kietgudy" target="_blank">
+              <GithubOutlined />
+              </Link>
+              <Link style={{fontSize: 22}} to="#">
+              <TwitterOutlined />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
